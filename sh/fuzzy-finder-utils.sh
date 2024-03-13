fd44b7817051809f31e2320444652a4f862efbbe
edisoncks <edison.chong.cks@gmail.com>
2024-03-13 12:09:53 +0800

    chore: refactor

--- sh/fuzzy-finder-utils.sh
+++ sh/fuzzy-finder-utils.sh
@@ -1,25 +1,29 @@
+#!/bin/sh
+exists() {
+	type "$1" >/dev/null 2>/dev/null
+}
+
 ff() {
-  find . -mindepth 1 -maxdepth 1 | fzf --reverse --border
+	fd . -d 1 -HI | fzf --reverse --border --preview 'if [ -d {} ]; then ls -lah --color=always {}; else bat --style=numbers --theme=Dracula --color=always {}; fi'
 }
 
 cdff() {
-  # Verifies if fzf executable is available
-  if ! command -v fzf &> /dev/null; then
-    echo "fzf executable not found"
-    return 1
-  fi
-  # Interactively change directory using fzf
-  tmp=$(find . -type d -mindepth 1 -maxdepth 1 | fzf --reverse --border --prompt='Change Directory: ')
-  [ -n "$tmp" ] && cd "$tmp"
+	# Verifies if fzf executable is available
+	if ! exists fzf; then
+		echo "fzf executable not found"
+		return 1
+	fi
+	# Interactively change directory using fzf
+	tmp=$(fd . -t d -d 1 -HI | fzf --reverse --border --prompt='Change Directory: ' --preview 'if [ -d {} ]; then ls -lah --color=always {}; else bat --style=numbers --theme=Dracula --color=always {}; fi')
+	[ -n "$tmp" ] && cd "$tmp" || return
 }
 
 rmff() {
-  # Verifies if fzf executable is available
-  if ! command -v fzf &> /dev/null; then
-    echo "fzf executable not found"
-    return 1
-  fi
-  # Interactively remove files using fzf
-  tmp=$(find . -mindepth 1 -maxdepth 1 | fzf --reverse --border --prompt='[Press TAB to Multi-Select] Remove Files: ' --multi)
-  [ -n "$tmp" ] && rm -Ir $(echo "$tmp" | tr '\n' ' ')
+	# Verifies if fzf executable is available
+	if ! exists fzf; then
+		echo "fzf executable not found"
+		return 1
+	fi
+	# Interactively remove files using fzf
+	fd . -d 1 -HI | fzf --reverse --border --prompt='[Press TAB to Multi-Select] Remove Files: ' --preview 'if [ -d {} ]; then ls -lah --color=always {}; else bat --style=numbers --theme=Dracula --color=always {}; fi' --multi --print0 | xargs -0 rm -rf
 }
